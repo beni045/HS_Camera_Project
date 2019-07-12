fe214df8c82300485bf729fc8b6090479e9e66df
Beni <benigans45@gmail.com>
2019-07-12 12:02:33 +0200

    Added ROI setting functions

--- Data_Collector.py
+++ Data_Collector.py
@@ -1,7 +1,12 @@
 import time
 from ximea import xiapi
 import numpy
-import PIL.Image 
+import PIL.Image
+import os
+
+#set path where images will be saved
+os.chdir('C:\\Users\\n.gerdes\\Documents\\HSCam_PythonApi\\Trial_Files\\Imgs')
+
 #create instance for first connected camera
 cam = xiapi.Camera()
 
@@ -14,12 +19,11 @@ cam.open_device()
 
 
 #settings
-
 cam.set_imgdataformat('XI_RAW16')
 cam.set_exposure(2500)
 print('Exposure was set to %i us' %cam.get_exposure())
 
-xiSetParamInt(handle, XI_PRM_OFFSET_X, 129)
+
 #create instance of Image to store image data and metadata
 img = xiapi.Image()
 
@@ -27,14 +31,17 @@ img = xiapi.Image()
 print('Starting data acquisition...')
 cam.start_acquisition()
 
-
+#set ROI
+cam.set_width(1000)
+cam.set_height(400)
+cam.set_offsetX(700)
+cam.set_offsetY(400)
 
 
 start_time = time.time()
 
 
-
-for x in range(170):
+for x in range(1000):
     #get data and pass them from camera to img
     cam.get_image(img)
 
@@ -55,19 +62,26 @@ for x in range(170):
     print('Image height (pixels): ' + str(img.height))
     print('First 10 pixels: ' + str(data[:10]))
     print('\n')"""
+
     
 print("--- %s seconds ---" % (time.time() - start_time))
 
+
 print(type(data_raw))
+
 
 #stop data acquisition
 print('Stopping acquisition...')
 cam.stop_acquisition()
 
+
 #stop communication
 cam.close_device()
-#print(type(data_raw))
-#img = PIL.Image.fromarray(data_raw, 'L')
-#img.show()
+
+
+print(type(data_raw))
+img = PIL.Image.fromarray(data_raw, 'L')
+img.show()
+
 
 print('Done.')
