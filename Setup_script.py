8e6d147f023b9bc6136157d1880f5a3aab20d0f7
beni045 <51675330+beni045@users.noreply.github.com>
2019-07-15 01:29:37 -0700

    Added function to create a folder for the Session with the format: (Session)_(Date)_(Sesssion_ID)

--- Setup_script.py
+++ Setup_script.py
@@ -11,10 +11,23 @@ import csv
 
 
 print("Hello")
-
+print("Make sure the correct path for the Session folder is set!")
+Check_if_set = input("Press enter to continue. Type anything to terminate.")
+if (Check_if_set != ""):
+    sys.exit()
 
 #create unique id for the session
 Session_ID = uuid.uuid4()
+
+#create a folder for Session
+Sesh_folder = "C:\\Users\\Beni\\Documents\\Ximea cam Python\\Testing_Makedirs\\Sessions"
+
+print(Sesh_folder)
+os.chdir("%s" %(Sesh_folder))
+d = datetime.datetime.today()    
+Date_sesh = d.strftime('%d-%m-%Y')
+if not os.path.exists('Session_%s_%s' %(Date_sesh,Session_ID)):
+       os.makedirs('Session_%s_%s' %(Date_sesh,Session_ID))
 
 
 #initialize variable for following block
